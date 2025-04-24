cd463cb69708808d786e101811af9075acb0c670
CRYSTIAN ANTÔNIO ROSSONI DUARTE <crystian_duarte@estudante.sesisenai.org.br>
2025-04-24 09:05:46 -0300

    Metodos especificos adcionados

--- script.js
+++ script.js
@@ -9,17 +9,29 @@ constructor (mana, vida, forca) {
 class guerreiro extends personagem {
 constructor (mana, vida, forca){
     super (mana, vida, forca )
+    } 
+atacar(){
+    console.log("Atacando")
 }
+
  }
 class mago extends personagem {
     constructor (mana, vida, forca){
         super (mana, vida, forca )
     }
+    usarMagia(){
+        console.log("UsandoMagia")
      }
+        
+    }
+
+
     class arqueiro extends personagem {
         constructor (mana, vida, forca){
             super (mana, vida, forca )
         }
-        
+        atirar(){
+            console.log("Atirando")
+        }
 
 }
