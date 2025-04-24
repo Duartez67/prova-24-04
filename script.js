86d2f5ececa9438bb3bd508085cefae2036b28a0
CRYSTIAN ANTÔNIO ROSSONI DUARTE <crystian_duarte@estudante.sesisenai.org.br>
2025-04-24 08:30:17 -0300

    tres classes filhas adcionadas

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-class personagens {
+class personagem {
 constructor (mana, vida, forca) {
     this.mana = mana,
     this.força = forca,
     this.vida = vida 
 }
+
+}
+class guerreiro extends personagem {
+constructor (mana, vida, forca){
+    super (mana, vida, forca )
+}
+ }
+class mago extends personagem {
+    constructor (mana, vida, forca){
+        super (mana, vida, forca )
+    }
+     }
+    class arqueiro extends personagem {
+        constructor (mana, vida, forca){
+            super (mana, vida, forca )
+        }
+        
 
 }
